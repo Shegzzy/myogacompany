46ae41bd012bbc61998c7594aff475a5f8121b73
softontechsolution <softontechsolution@gmail.com>
2023-04-03 14:37:01 +0100

    tracking added

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,40 +1,149 @@
 import "./featured.scss";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 import { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, where, query, onSnapshot } from "firebase/firestore";
 import { db } from "../../firebase";
 
 const Featured = () => {
-  const [totalPayments, setTotalPayments] = useState(0);
+  const [Selected, setSelected] = useState("total");
+  const [data, setData] = useState([]);
+  const [oData, setOData] = useState([]);
+  const [lData, setLData] = useState([]);
+  const [fieldSum, setFieldSum] = useState(0);
+  const [oneSum, setOneSum] = useState(0);
+  const [lastSum, setLastSum] = useState(0);
+  const [diff, setDiff] = useState(null);
 
   useEffect(() => {
-    const sumPrice = async () => {
-      const querySnapshot = await getDocs(collection(db, "Bookings"));
-      let total = 0;
-      querySnapshot.forEach((doc) => {
-        const data = doc.data();
-        total += parseFloat(data.Amount);
-      });
-      setTotalPayments(total);
+
+    const FetchData = async () => {
+
+      let list = [];
+
+      if (Selected === "1") {
+        const today = new Date();
+        const yesterday = new Date(today.getTime() - 24 * 60 * 60 * 1000); // Timestamp for 1 day ago // 24 hours in milliseconds
+        const q = query(collection(db, "Bookings"), where("timeStamp", "<=", today), where("timeStamp", ">", yesterday));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+          // doc.data() is never undefined for query doc snapshots
+        });
+        setData(list);
+
+      } else if (Selected === "7") {
+        const now = new Date();
+        const sevenDaysAgo = new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000);
+        const q = query(collection(db, "Bookings"), where("timeStamp", ">=", sevenDaysAgo));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+          // doc.data() is never undefined for query doc snapshots
+        });
+        setData(list);
+
+      } else if (Selected === "30") {
+        const now = new Date();
+        const thirtyDaysAgo = new Date(now.getTime() - 30 * 24 * 60 * 60 * 1000);
+        const q = query(collection(db, "Bookings"), where("timeStamp", ">=", thirtyDaysAgo));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+          // doc.data() is never undefined for query doc snapshots
+        });
+        setData(list);
+      } else if (Selected === "total") {
+        const unsub = onSnapshot(collection(db, "Bookings"), (snapShot) => {
+          let list = [];
+          snapShot.docs.forEach(doc => {
+            list.push({ id: doc.id, ...doc.data() });
+          });
+          setData(list);
+
+        }, (error) => {
+          alert("Error", + error.message);
+        });
+
+        return () => {
+          unsub();
+        }
+      }
     };
-    sumPrice();
-  }, []);
+    FetchData();
+
+  }, [Selected]);
+
+  useEffect(() => {
+    const sum = data.reduce((total, item) => total + parseInt(item.Amount), 0);
+    setFieldSum(sum);
+  }, [data]);
+
+  useEffect(() => {
+    getData();
+  });
+
+  const getData = async () => {
+    let dataArray = [];
+    let dataOArray = [];
+    const today = new Date();
+    const oneWeekAgo = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
+    const twoWeekAgo = new Date(today.getTime() - 14 * 24 * 60 * 60 * 1000);
+    const lastMonth = new Date(new Date().setMonth(today.getMonth() - 1));
+    const prevMonth = new Date(new Date().setMonth(today.getMonth() - 2));
+
+    const lastMonthQuery = query(collection(db, "Bookings"), where("timeStamp", "<=", today), where("timeStamp", ">", lastMonth));
+    const prevMonthQuery = query(collection(db, "Bookings"), where("timeStamp", "<=", lastMonth), where("timeStamp", ">", prevMonth));
+    const oneWeekQuery = query(collection(db, "Bookings"), where("timeStamp", "<=", today), where("timeStamp", ">", oneWeekAgo));
+    const twoWeekQuery = query(collection(db, "Bookings"), where("timeStamp", "<=", oneWeekAgo), where("timeStamp", ">", twoWeekAgo));
+
+    const lastMonthData = await getDocs(lastMonthQuery);
+    const prevMonthData = await getDocs(prevMonthQuery);
+    const oneWeekData = await getDocs(oneWeekQuery);
+    const twoWeekData = await getDocs(twoWeekQuery);
+    setDiff((oneWeekData.docs.length - twoWeekData.docs.length) / (twoWeekData.docs.length) * 100);
+
+    lastMonthData.forEach((doc) => {
+      dataArray.push({ id: doc.id, ...doc.data() });
+    });
+    setLData(dataArray);
+
+    oneWeekData.forEach((doc) => {
+      dataOArray.push({ id: doc.id, ...doc.data() });
+    });
+    setOData(dataOArray);
+
+    const total = lData.reduce((total, item) => total + parseInt(item.Amount), 0);
+    setLastSum(total);
+
+    const sum = oData.reduce((total, item) => total + parseInt(item.Amount), 0);
+    setOneSum(sum);
+
+  }
+
+
   return (
     <div className="featured">
       <div className="top">
         <h1 className="title">Company's Income</h1>
-        <MoreVertIcon fontSize="small" />
+        <select className="chart-select" onChange={(e) => {
+          e.preventDefault();
+          setSelected(e.target.value);
+        }} >
+          <option value="total">Total</option>
+          <option value="30">30 Days</option>
+          <option value="7">7 Days</option>
+          <option value="1">1 Day</option>
+        </select>
       </div>
       <div className="bottom">
         <div className="featuredChart">
-          <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
+          <CircularProgressbar value={diff} text={`${diff}%`} strokeWidth={5} />
         </div>
         <p className="title">Total Earnings</p>
-        <p className="amount">₦ {totalPayments}</p>
+        <p className="amount">₦{fieldSum}</p>
         <p className="desc">
           Previous transactions processing. Last payments may not be included.
         </p>
@@ -43,21 +152,21 @@ const Featured = () => {
             <div className="itemTitle">Target</div>
             <div className="itemResult negative">
               <KeyboardArrowDownIcon fontSize="small" />
-              <div className="resultAmount">$12.4k</div>
+              <div className="resultAmount">₦12000</div>
             </div>
           </div>
           <div className="item">
             <div className="itemTitle">Last Week</div>
             <div className="itemResult positive">
               <KeyboardArrowUpOutlinedIcon fontSize="small" />
-              <div className="resultAmount">$12.4k</div>
+              <div className="resultAmount">${oneSum}</div>
             </div>
           </div>
           <div className="item">
             <div className="itemTitle">Last Month</div>
             <div className="itemResult positive">
               <KeyboardArrowUpOutlinedIcon fontSize="small" />
-              <div className="resultAmount">$12.4k</div>
+              <div className="resultAmount">${lastSum}</div>
             </div>
           </div>
         </div>
--- src/pages/bookings/bookings.jsx
+++ src/pages/bookings/bookings.jsx
@@ -13,6 +13,7 @@ import { useEffect, useState } from "react";
 import { db } from "../../firebase";
 import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
+import ModalContainer from "../../components/modal/ModalContainer";
 
 const Bookings = ({ inputs, title }) => {
   const [data, setData] = useState([]);
@@ -45,90 +46,101 @@ const Bookings = ({ inputs, title }) => {
         <div className="top">
           <h1>{title}</h1>
         </div>
-        <TableContainer component={Paper} className="table">
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell className="tableCell" width={80}>
-                  Booking Number
-                </TableCell>
-                <TableCell className="tableCell" width={80}>
-                  Package
-                </TableCell>
-                <TableCell className="tableCell" width={130}>
-                  Customer Name
-                </TableCell>
-                <TableCell className="tableCell" width={130}>
-                  Date
-                </TableCell>
-                <TableCell className="tableCell" width={50}>
-                  Amount
-                </TableCell>
-                <TableCell className="tableCell" width={20}>
-                  Payment Method
-                </TableCell>
-                <TableCell className="tableCell" width={130}>
-                  Pickup Location
-                </TableCell>
-                <TableCell className="tableCell" width={130}>
-                  Dropoff Location
-                </TableCell>
-                <TableCell className="tableCell" width={130}>
-                  Phone
-                </TableCell>
-                <TableCell className="tableCell" width={130}>
-                  Driver ID
-                </TableCell>
-                <TableCell className="tableCell" width={130}>
-                  Distance
-                </TableCell>
-                <TableCell className="tableCell" width={130}>
-                  Ride Type
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map((row) => (
-                <TableRow key={row.id}>
+        <div className="b-table">
+          <TableContainer component={Paper} className="table">
+            <Table sx={{ minWidth: 780 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
                   <TableCell className="tableCell" width={80}>
-                    {row["Booking Number"]}
+                    Booking Number
                   </TableCell>
-                  <TableCell className="tableCell">
-                    {row["Package Type"]}
+                  <TableCell className="tableCell" width={80}>
+                    Package
                   </TableCell>
-                  <TableCell className="tableCell">
-                    {row["Customer Name"]}
+                  <TableCell className="tableCell" width={130}>
+                    Customer Name
                   </TableCell>
-                  <TableCell className="tableCell">
-                    {new Date(row["Date Created"]).toLocaleDateString("en-US")}
+                  <TableCell className="tableCell" width={130}>
+                    Date
                   </TableCell>
-                  <TableCell className="tableCell">{row.Amount}</TableCell>
-                  <TableCell className="tableCell">
-                    {row["Payment Method"]}
+                  <TableCell className="tableCell" width={50}>
+                    Amount
                   </TableCell>
-                  <TableCell className="tableCell">
-                    {row["PickUp Address"]}
+                  <TableCell className="tableCell" width={20}>
+                    Payment Method
                   </TableCell>
-                  <TableCell className="tableCell">
-                    {row["DropOff Address"]}
+                  <TableCell className="tableCell" width={130}>
+                    Pickup Location
                   </TableCell>
-                  <TableCell className="tableCell">
-                    {row["Customer Phone"]}
+                  <TableCell className="tableCell" width={130}>
+                    Dropoff Location
                   </TableCell>
-                  <TableCell className="tableCell">
-                    <Link to={`/users/${row["Driver ID"]}`}>
-                      {row["Driver ID"]}
-                    </Link>
+                  <TableCell className="tableCell" width={130}>
+                    Phone
                   </TableCell>
-                  <TableCell className="tableCell">{row["Distance"]}</TableCell>
-                  <TableCell className="tableCell">
-                    {row["Ride Type"] === "1" ? "Car" : "Motorcycle"}
+                  <TableCell className="tableCell" width={130}>
+                    Driver ID
+                  </TableCell>
+                  <TableCell className="tableCell" width={80}>
+                    Distance
+                  </TableCell>
+                  <TableCell className="tableCell" width={130}>
+                    Ride Type
+                  </TableCell>
+                  <TableCell className="tableCell" width={100}>
+                    Status
                   </TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {data.map((row) => (
+                  <TableRow key={row.id}>
+                    <TableCell className="tableCell" width={80}>
+                      {row["Booking Number"]}
+                    </TableCell>
+                    <TableCell className="tableCell">
+                      {row["Package Type"]}
+                    </TableCell>
+                    <TableCell className="tableCell">
+                      {row["Customer Name"]}
+                    </TableCell>
+                    <TableCell className="tableCell">
+                      {new Date(row["Date Created"]).toLocaleDateString("en-US")}
+                    </TableCell>
+                    <TableCell className="tableCell">{row.Amount}</TableCell>
+                    <TableCell className="tableCell">
+                      {row["Payment Method"]}
+                    </TableCell>
+                    <TableCell className="tableCell">
+                      {row["PickUp Address"]}
+                    </TableCell>
+                    <TableCell className="tableCell">
+                      {row["DropOff Address"]}
+                    </TableCell>
+                    <TableCell className="tableCell">
+                      {row["Customer Phone"]}
+                    </TableCell>
+                    <TableCell className="tableCell">
+                      <Link to={`/users/${row["Driver ID"]}`}>
+                        {row["Driver ID"]}
+                      </Link>
+                    </TableCell>
+                    <TableCell className="tableCell">{row["Distance"]}</TableCell>
+                    <TableCell className="tableCell">
+                      {row["Ride Type"] === "1" ? "Truck" : "Motorcycle"}
+                    </TableCell>
+                    <TableCell className="tableCell">
+                      <div className={`cellWithStatus ${row["Status"]}`}>
+                        {row["Status"]}
+                        {<ModalContainer id={row.id} />}
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       </div>
     </div>
   );

600c19e776b8c0e3c6257e29236511da1100aade
Shegzzy <miksegun1996@gmail.com>
2023-08-04 10:30:35 +0100

    handled reload error and few updates

--- src/components/status/OrderStatus.jsx
+++ src/components/status/OrderStatus.jsx
@@ -135,15 +135,15 @@ const OrderStatus = (props) => {
         <div className="flex-1">
           <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
             <img
-              className="object-cover w-full h-64 rounded-md"
+              className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
               src={Ddata.map((Ddata) => Ddata.img)}
-              alt="Driver"
-              style={{
-                objectFit: "cover",
-                width: "100%",
-                height: "300px",
-                borderRadius: "10px",
-              }}
+              alt="Rider's pic"
+              // style={{
+              //   objectFit: "cover",
+              //   width: "100%",
+              //   height: "300px",
+              //   borderRadius: "10px",
+              // }}
             />
             <div className="text-center space-y-2 sm:text-left">
               <div className="space-y-0.5">
@@ -174,13 +174,13 @@ const OrderStatus = (props) => {
             <img
               className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
               src={Cdata.map((Cdata) => Cdata.img)}
-              alt="Customer"
-              style={{
-                objectFit: "cover",
-                width: "100%",
-                height: "300px",
-                borderRadius: "10px",
-              }}
+              alt="Customer's pic"
+              // style={{
+              //   objectFit: "cover",
+              //   width: "100%",
+              //   height: "300px",
+              //   borderRadius: "10px",
+              // }}
             />
             <div className="text-center space-y-2 sm:text-left">
               <div className="space-y-0.5">

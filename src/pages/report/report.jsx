a58ad2cc9d3fe5f5c7db939fff32cfb9e3cc2537
Segun <miksegun1996@gmail.com>
2023-08-15 20:25:22 +0100

    adding profit and earnings

--- src/pages/report/report.jsx
+++ src/pages/report/report.jsx
@@ -4,6 +4,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import ReportChart from "../../components/report-charts/report_chart";
 import CompletedBookingsChart from "../../components/report-charts/completed_bookings_chart";
+import EarningProfits from "../../components/report-charts/monthly_earnings_and_profits";
 import Chart from "../../components/chart/Chart";
 
 const Report = () => {
@@ -20,6 +21,7 @@ const Report = () => {
             aspect={4 / 3}
           />
           <Chart title="Total Earnings Chart" aspect={2 / 1} />
+          <EarningProfits title="Monthly Earnings and Profits" />
         </div>
       </div>
     </div>
